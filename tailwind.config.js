8009859f9bcd2cab5dad1df2b92a626c17cbb420
akorede <emiolasaheed1@gmail.com>
2024-02-16 23:13:56 +0100

    Set up app colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg--light': '#fff',
+        'bg--dark': '#262e35',
+        'sidebar--light': '#F5F7FB',
+        'sidebar--dark': '#303841',
+        'sender--bg--dark': '#262e35',
+        'message--cirle': '#523b49',
+        'message--text': '#ef4669',
+        'message--bg--blue': '#7269ef',
+        'bg--silver': '#e6ebf5'
+      }
+    },
   },
   plugins: [],
 }
