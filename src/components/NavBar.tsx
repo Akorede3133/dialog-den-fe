3ac062721025517236d4f01e773f73a18ddb0d80
akorede <emiolasaheed1@gmail.com>
2024-02-17 11:33:44 +0100

    Add logo to header

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,16 @@
 import { CiUser } from "react-icons/ci";
 import { HiOutlineChatBubbleOvalLeftEllipsis, HiOutlineUser, HiOutlineUserCircle, HiOutlineSun,  HiOutlineCog8Tooth, HiOutlineMoon  } from "react-icons/hi2";
 import NavIcon from "./NavIcon";
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
   return (
-    <header className="fixed bottom-0 sm:top-0 left-0 w-full sm:w-[5rem] bg-white drop-shadow-xl shadow-inner sm:px-4">
+    <header className="fixed bottom-0 sm:top-0 left-0 w-full sm:w-[5rem] bg-white drop-shadow-xl shadow-inner">
+      <section className="hidden sm:block my-10 px-3">
+        <img src={logo} className=" w-[70px] h-auto object-cover rounded-md" alt="" />
+      </section>
       <nav>
-        <ul className="flex sm:flex-col justify-between sm:gap-10">
+        <ul className="flex items-center sm:flex-col justify-between sm:gap-10 px-4">
           <NavIcon link='profile' icon={<HiOutlineUserCircle className="text-xl hover:text-primary-blue bg-inherit text-inherit"  />} />
           <NavIcon link='' icon={<HiOutlineChatBubbleOvalLeftEllipsis className="text-xl hover:text-primary-blue bg-inherit text-inherit"  />} />
           <NavIcon link='contacts' icon={<HiOutlineUser className="text-xl hover:text-primary-blue bg-inherit text-inherit"  />} />
@@ -15,6 +19,9 @@ const NavBar = () => {
             <button className="p-4">
               <HiOutlineMoon  className="text-xl hover:text-primary-blue bg-inherit text-inherit" />
             </button>
+          </li>
+          <li>
+            <img src={logo} alt="" className="w-[30px] h-[30px] rounded-full" />
           </li>
         </ul>
       </nav>
