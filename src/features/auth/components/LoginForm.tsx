d614b18752dbad5121ca03c963571339305d9989
akorede <emiolasaheed1@gmail.com>
2024-02-19 18:53:34 +0100

    Create login form

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,19 +1,9 @@
-import { HiOutlineMail } from "react-icons/hi"
 import { HiOutlineLockClosed, HiOutlineUser } from "react-icons/hi2"
 import { Link } from "react-router-dom"
 
-const RegisterForm = () => {
+const LoginForm = () => {
   return (
     <form className="bg-white rounded-md shadow-md p-4 w-[90%] sm:w-[500px] mx-auto space-y-3 my-10">
-      <section className=" space-y-2">
-        <label htmlFor="email" className=" text-sm text-text-primary font-semibold">Email</label>
-        <div className="border flex gap-3 rounded-md">
-          <button className="bg-bg-silver p-3">
-            <HiOutlineMail className=" text-text-gray" />
-          </button>
-          <input type="email" placeholder="Enter Email" className=" outline-none text-sm" />
-        </div>
-      </section>
       <section className=" space-y-2">
         <label htmlFor="username" className=" text-sm text-text-primary font-semibold">Username</label>
         <div className="border flex gap-3 rounded-md">
@@ -36,11 +26,11 @@ const RegisterForm = () => {
         <button className="capitalize bg-message-bg-blue w-full text-white py-2 rounded-md">Register</button>
       </section>
       <section className="text-center space-y-3 pt-5">
-        <p>Already have an account? <Link to='/login' className="text-message-bg-blue">Signin</Link></p>
+        <p>Don&apos;t have an account? <Link to='/login' className="text-message-bg-blue">register</Link></p>
         <p>&copy; 2024 Dialogden</p>
       </section>
     </form>
   )
 }
 
-export default RegisterForm
+export default LoginForm
--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -1,9 +1,11 @@
 import AuthHeader from "../components/AuthHeader"
+import LoginForm from "../components/LoginForm"
 
 const Login = () => {
   return (
     <div  className=" bg-bg-silver flex flex-col items-center justify-center min-h-screen py-10">
       <AuthHeader action="Signin" description="Signin to your DialogueDen account." />
+      <LoginForm />
     </div>
   )
 }
