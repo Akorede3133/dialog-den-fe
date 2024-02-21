072f010346a92c5b6afa6aa273021d3a41ee1421
akorede <emiolasaheed1@gmail.com>
2024-02-21 17:54:04 +0100

    Create api function  to login user

--- src/features/auth/api/auth..ts
+++ src/features/auth/api/auth..ts
@@ -8,8 +8,29 @@ type RegisterProp = {
 
 export const register = async (data: RegisterProp) => {
   try {
-      console.log(data);
     const response = await fetch(`${API_URL}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const result = await response.json();
+
+    if(!response.ok) {
+      throw new Error(result.message);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message)
+    }
+  }
+ 
+}
+
+export const login = async (data: RegisterProp) => {
+  try {
+    const response = await fetch(`${API_URL}/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
