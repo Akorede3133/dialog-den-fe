c5c1736ba3fa69ff1f908227c4128348bfb5e1c7
akorede <emiolasaheed1@gmail.com>
2024-02-21 22:23:06 +0100

    Create hook to get loggedin user

---
+++ src/features/auth/hooks/useCurrentUser.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query"
+import { currentUser } from "../api/auth."
+
+const useCurrentUser = () => {
+  const { data: user, isPending: isGettingUser, error } = useQuery({
+    queryFn: () => currentUser(),
+    queryKey: ['user']
+  });
+  return { user, isGettingUser, error };
+}
+
+export default useCurrentUser;
