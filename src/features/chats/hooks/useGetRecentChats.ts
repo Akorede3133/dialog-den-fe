ae62a9bec9b253a448f390edfed0c6ded165551b
akorede <emiolasaheed1@gmail.com>
2024-02-24 22:58:07 +0100

    Create hook to get recent chats

---
+++ src/features/chats/hooks/useGetRecentChats.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query"
+import getRecentChats from "../api/getRecentChats"
+
+const useGetRecentChats = () => {
+  const { data: chats, isPending:isGettingChats, error } = useQuery({
+    queryFn: () => getRecentChats(),
+    queryKey: ['recentChats']
+  })
+
+  return { chats, isGettingChats, error };
+}
+
+export default useGetRecentChats
