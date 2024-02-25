23432644dcc481d66006a2a7ccef46edd42421f6
akorede <emiolasaheed1@gmail.com>
2024-02-25 07:06:45 +0100

    Display recent chats

--- src/features/chats/components/RecentChatCard.tsx
+++ src/features/chats/components/RecentChatCard.tsx
@@ -1,6 +1,23 @@
 import logo from '../../../assets/logo.png';
+import { formatTime } from '../../../utils/dateTime';
 
-const RecentChatCard = () => {
+export type ChatProp =  {
+  messageId: number;
+  content: string;
+  type: string;
+  createdAt: string;
+  updatedAt: string;
+  senderId: number;
+  receiverId: number;
+  msgSenderId?: number;
+  msgReceiverId?: number;
+  senderUsername?: string;
+  receiverUsername?: string;
+  senderEmail?: string;
+  receiverEmail?: string;
+};
+const RecentChatCard = ({ chat }: { chat: ChatProp }) => {
+  const { senderUsername, receiverUsername, content, createdAt } = chat;
   return (
     <li className="flex justify-between items-center">
       <section className="flex items-center gap-4">
@@ -9,11 +26,11 @@ const RecentChatCard = () => {
           <span className='bg-green-500 h-[10px] w-[10px] rounded-full border border-white absolute top-[50%] right-0'></span>
         </div>
         <div className="flex flex-col">
-          <span>Patrick Hendricks</span>
-          <span className="text-sm text-text-gray">Hey! there I'm available</span>
+          <span>{senderUsername || receiverUsername}</span>
+          <span className="text-sm text-text-gray">{content}</span>
         </div>
       </section>
-      <span className="text-[12px] text-text-gray">02:50PM</span>
+      <span className="text-[12px] text-text-gray">{formatTime(createdAt)}</span>
     </li>
   )
 }
--- src/features/chats/components/RecentChats.tsx
+++ src/features/chats/components/RecentChats.tsx
@@ -1,23 +1,23 @@
-import RecentChatCard from "./RecentChatCard"
+import useGetRecentChats from "../hooks/useGetRecentChats"
+import RecentChatCard, { ChatProp } from "./RecentChatCard"
 
 const RecentChats = () => {
+  const { chats, isGettingChats, error } = useGetRecentChats();
+
+  if (isGettingChats) {
+    return <p>Loading...</p>
+  }
+  if (error) {
+    return <p>{error.message}</p>
+  }
   return (
     <div className=" recent--chats overflow-auto max-h-[350px] py-5">
       <ul className="space-y-5 px-5">
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
-        <RecentChatCard />
+        {
+          chats.map((chat: ChatProp) => (
+            <RecentChatCard key={chat.id} chat={chat} />
+          ))
+        }
       </ul>
     </div>
   )
