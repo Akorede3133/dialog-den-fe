e87e8677759f6e248efba0674128d1000c943908
akorede <emiolasaheed1@gmail.com>
2024-03-05 18:45:14 +0100

    Indicate voice note as last message sent or receieved for recent chats

--- src/features/chats/components/RecentChatCard.tsx
+++ src/features/chats/components/RecentChatCard.tsx
@@ -1,4 +1,4 @@
-import { FaPhotoFilm } from 'react-icons/fa6';
+import { FaMicrophone, FaPhotoFilm } from 'react-icons/fa6';
 import logo from '../../../assets/logo.png';
 import { useAppDispatch } from '../../../redux/hooks';
 import { formatTime } from '../../../utils/dateTime';
@@ -6,6 +6,9 @@ import { UserProp } from '../../contacts/components/ContactCard';
 import { setReceiver } from '../redux/chatSlice';
 import { HiPhoto } from 'react-icons/hi2';
 import { useSocketContext } from '../context/socketContext';
+import { useEffect, useRef, useState } from 'react';
+import WaveSurfer from 'wavesurfer.js';
+import formatDuration from '../../../utils/formatDuration';
 
 export type ChatProp =  {
   messageId: number;
@@ -23,6 +26,12 @@ export type ChatProp =  {
   receiverEmail?: string;
 };
 const RecentChatCard = ({ chat }: { chat: ChatProp }) => {
+  const [totalDuration ,setTotalDuration] = useState
+  (0);
+  const [waveForm, setWaveForm] = useState<WaveSurfer | null>(null) 
+  
+  const waveFormRef = useRef<HTMLDivElement>(null)
+  
   const { senderUsername, receiverUsername, content, type, msgSenderId, msgReceiverId, senderEmail, receiverEmail, createdAt } = chat;
   const dispatch = useAppDispatch();
   const { onlineUsers } = useSocketContext();
@@ -34,6 +43,22 @@ const RecentChatCard = ({ chat }: { chat: ChatProp }) => {
     email: senderEmail || receiverEmail,
     username: senderUsername || receiverUsername
   } as UserProp;
+
+  useEffect(() => {
+    if (waveForm) {
+      waveForm.load(content)
+      waveForm.on('ready', () => {
+        setTotalDuration(waveForm.getDuration() as number)
+      })
+    }
+  }, [waveForm, content])
+  
+  useEffect(() => {
+    const waveSurfer = WaveSurfer.create({
+      container: waveFormRef.current as HTMLDivElement,
+    })
+    setWaveForm(waveSurfer)
+  }, [])
   return (
     <li className="flex cursor-pointer justify-between items-center"  onClick={() => dispatch(setReceiver(user))}>
       <section className="flex items-center gap-4">
@@ -53,9 +78,21 @@ const RecentChatCard = ({ chat }: { chat: ChatProp }) => {
               <HiPhoto className=' text-text-gray' />
             </div>
           }
+            {
+            type === 'voice' && <div className=' flex items-center gap-3'>
+              <span className='text-sm'>
+                <FaMicrophone className=' text-text-gray text-[0.7rem]' />
+              </span>
+              <p className='text-[0.7rem]'>
+                {formatDuration(totalDuration)}
+              </p>
+            </div>
+          }
          
         </div>
       </section>
+      <div ref={waveFormRef} hidden></div>
+
       <span className="text-[12px] text-text-gray">{formatTime(createdAt)}</span>
     </li>
   )
