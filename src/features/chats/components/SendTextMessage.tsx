4475baf0f110d154cf5e8cab2924e7b5ca948169
akorede <emiolasaheed1@gmail.com>
2024-03-09 07:10:20 +0100

    Create emoji context menu

--- src/features/chats/components/SendTextMessage.tsx
+++ src/features/chats/components/SendTextMessage.tsx
@@ -3,7 +3,7 @@ import { HiOutlineMicrophone, HiOutlinePhoto, HiPaperAirplane } from "react-icon
 import { useAppSelector } from "../../../redux/hooks"
 import { selectChat } from "../redux/chatSlice"
 import useSendMessage from "../hooks/useSendMessage"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import useSendImage from "../hooks/useSendImage"
 import { useQueryClient } from "@tanstack/react-query";
 import Picker from "emoji-picker-react";
@@ -12,9 +12,10 @@ type TextMessageProps = {
   showRecorder: () => void
 }
 const SendTextMessage = ({ showRecorder }: TextMessageProps) => {
+  const [showEmoji, setShowEmoji] = useState(false);
   const { receiver } = useAppSelector(selectChat);
   const queryClient = useQueryClient();
-
+  const emojiRef = useRef<HTMLDivElement>(null)
   const { send, isSending } = useSendMessage();
   const { sendImageFile, isSendingImage, error } = useSendImage();
   const [message, setMessage] = useState<string>('');
@@ -29,7 +30,6 @@ const SendTextMessage = ({ showRecorder }: TextMessageProps) => {
 
   const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
-    // adjustTextArea(e.target)
   }
 
   const handleSend = () => {
@@ -55,16 +55,33 @@ const SendTextMessage = ({ showRecorder }: TextMessageProps) => {
       `${prevMessage}${emoji.emoji}`
     ))    
   }
+
+  useEffect(() => {
+    const emojiEvent = (e) => {
+      if (emojiRef.current && !emojiRef.current.contains(e.target)) {
+        console.log('event');
+        setShowEmoji(false);
+      }
+    }
+    document.addEventListener('click', emojiEvent, true)
+    return () => {
+      document.addEventListener('click', emojiEvent, true);
+    }
+  }, [])
+
+  const handleEmoji = () => {
+    setShowEmoji((prev) => !prev)
+  }
   return (
     <div className="bg-white relative grid grid-cols-[1fr,auto] items-center gap-4 p-3">
-      <div className=" absolute bottom-[100px] right-20">
-        <Picker onEmojiClick={onEmojiClick} />
-      </div>
+      { showEmoji && <div ref={emojiRef} className=" absolute bottom-[100px] right-20">
+        <Picker onEmojiClick={onEmojiClick} /> 
+      </div> }
       <section className="">
         <textarea placeholder="Enter Message..."  className=" resize-none h-[50px] bg-bg-silver p-3 w-full rounded-md outline-none overflow-hidden" value={message} onChange={handleInput } onKeyDown={ adjustTextArea}  rows={1}/>
       </section>
       <section className="flex gap-5 items-center">
-        <button>
+        <button onClick={handleEmoji}>
           <HiOutlineEmojiHappy className=" text-message-bg-blue" />
         </button>
         <label htmlFor="image" className=" cursor-pointer">
