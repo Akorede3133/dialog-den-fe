6a8f0fcb80640d4b407992fff5556dc523f47053
akorede <emiolasaheed1@gmail.com>
2024-03-23 07:18:44 +0100

    Style voice calll UI

--- src/features/chats/components/VoiceCall.tsx
+++ src/features/chats/components/VoiceCall.tsx
@@ -1,6 +1,18 @@
+import { FaPhone } from 'react-icons/fa6';
+import logo from '../../../assets/logo.png';
+
 const VoiceCall = () => {
   return (
-    <div>VoiceCall</div>
+    <div className=" bg-message-bg-blue min-h-screen flex flex-col justify-center items-center gap-20">
+      <div className=' text-center'>
+        <p className=' text-2xl text-white'>Tracy</p>
+        <span className='text-white text-sm'>calling...</span>
+      </div>
+      <img src={logo} alt="" className='w-[150px] h-[150px] rounded-full' />
+      <button className='bg-red-500 p-4 rounded-full  animate-pulse'>
+        <FaPhone className=' text-white' />
+      </button>
+    </div>
   )
 }
 
