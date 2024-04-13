0e6ebd3527a70440e2413b57f2958af3368deccc
akorede <emiolasaheed1@gmail.com>
2024-04-13 14:38:26 +0100

    Create API functipon to update profile

---
+++ src/features/profile/api/updateProfile.ts
@@ -0,0 +1,27 @@
+import { API_URL } from "../../../utils/constants"
+
+export const updateProfile = async (userId, data) => {
+  try {
+    const formData = new FormData();
+    formData.append('username', data.username);
+    formData.append('photo', data.photo);
+    formData.append('password', data.password);
+    formData.append('passwordConfirmation', data.passwordConfirmation);
+    const response = await fetch(`${API_URL}/updateUser${userId}`, {
+      method: 'PUT',
+      credentials: 'include',
+      body: formData
+    });
+    const result = await response.json();
+    
+    if (!response.ok) {
+      throw new Error(result.message);
+    }
+  } catch (error) {
+    if (error) {
+      throw new Error(error.message);
+
+    }
+    
+  }
+}
