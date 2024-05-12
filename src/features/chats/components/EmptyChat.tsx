1c6d553713568f38fcb791cfb47a009383ab3def
akorede <emiolasaheed1@gmail.com>
2024-05-12 06:50:25 +0100

    Display an image to indication empty conversation

--- src/features/chats/components/EmptyChat.tsx
+++ src/features/chats/components/EmptyChat.tsx
@@ -1,6 +1,12 @@
+import emptyConvoImg from '../../../assets/empty_convo.png';
+
 const EmptyChat = () => {
   return (
-    <div className="hidden sm:block">EmptyChat</div>
+    <div className="hidden sm:bloc h-full sm:flex items-center justify-center">
+      <div>
+        <img src={emptyConvoImg} alt="empty conversation" />
+      </div>
+    </div>
   )
 }
 
