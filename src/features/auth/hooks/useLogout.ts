0c60d2583b5970a8bc9702bcae9964eed0975f8f
akorede <emiolasaheed1@gmail.com>
2024-05-14 07:11:31 +0100

    Create hook for logingout user

---
+++ src/features/auth/hooks/useLogout.ts
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+// import { useNavigate } from "react-router-dom";
+import { logout } from "../api/auth";
+import toast from "react-hot-toast";
+
+const useLogout = () => {
+  // const navigate = useNavigate();
+  const { mutate: logoutUser, isPending: isLoggingOut, error } = useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      // navigate('/');
+      toast.success('LoggedOut sucessfully!!')
+    }
+  });
+
+  return { logoutUser, isLoggingOut, error }
+}
+
+export default useLogout;
