55b5fb4d35aafe3c0a99ae16cbdc9073dcfe357d
akorede <emiolasaheed1@gmail.com>
2024-05-17 07:14:42 +0100

    Indicate settings page still under development

--- src/features/settings/Settings.tsx
+++ src/features/settings/Settings.tsx
@@ -1,6 +1,9 @@
 const Settings = () => {
   return (
-    <div className="h-screen overflow-hidden sm:order-2">Settings</div>
+    <div className="h-screen overflow-hidden sm:order-2 text-xl flex flex-col items-center justify-center">
+      <p>SETTINGS</p>
+      <p>Still under development</p>
+    </div>
   )
 }
 
